794d4e80bb221c6845e14a0a2b26104f6299a3c9
Richard Pascual <ricpascual@gmail.com>
2023-02-01 19:43:27 -0500

    callback and promises

---
+++ src/promise/index.js
@@ -0,0 +1,20 @@
+const promise = new Promise(function (resolve, reject) {
+    resolve("lokaso");
+})
+
+
+cows = 9;
+const counCowns = new Promise(function (resolve, reject) {
+    if (cows > 10){
+        resolve(`we have ${cows}`);
+    }else{
+        reject("there is no cowns on the farm");
+    }
+    
+})
+
+counCowns.then((result)=>{
+    console.log(result);
+}).catch((err)=>{
+    console.log(err);
+})
